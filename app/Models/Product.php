a14c8c30b2fd3c1abef0e0b1c7047633721c7a48
Barbosa <omar.barbosa@evertecinc.com>
2022-01-28 16:30:36 -0500

    Chore: Remove relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,49 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['slug', 'description', 'price'];
-
-    public function setDescriptionAttribute($value)
-    {
-        $this->attributes['description'] = ucfirst(strtolower($value));
-    }
-
-    public function getDescriptionAttribute($value)
-    {
-        return $this->attributes['description'] = strtoupper($value);
-    }
-
-    public function category(): BelongsTo
-    {
-        return $this->belongsTo(ProductCategory::class, 'category_id');
-    }
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function images(): HasMany
-    {
-        return $this->hasMany(Image::class);
-    }
-
-    public function invoices(): BelongsToMany
-    {
-        return $this->belongsToMany(Invoice::class);
-    }
-
-    public function image(): HasOne
-    {
-        return $this->hasOne(Image::class)->latestOfMany();
-    }
 }
