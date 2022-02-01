3a8374851e885b9e1295aa22fa17e07face56589
Barbosa <omar.barbosa@evertecinc.com>
2022-02-01 09:25:45 -0500

    Refactor: Add cart routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ShoppingCartController;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +25,16 @@ Route::prefix('admin')
         require_once __DIR__ . '/admin/products.php';
     });
 
+Route::post('pay', [PaymentController::class, 'store'])->name('payments.store')
+;
+Route::delete('cart', [ShoppingCartController::class, 'destroy'])->name('cart.destroy');
+Route::post('cart', [ShoppingCartController::class, 'store'])->name('cart.store');
+Route::get('cart', [ShoppingCartController::class, 'index'])->name('cart.index');
+
+Route::get('products/{slug}', [ProductController::class, 'show'])->name('products.show');
+
+Route::get('/', [WelcomeController::class, 'index']);
+
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
