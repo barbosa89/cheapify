3c2947deb2549d97ab10695e765960543699a725
Barbosa <omar.barbosa@evertecinc.com>
2022-02-25 15:26:48 -0500

    Chore: Compile Vue 2, publish source maps in local

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .vue()
+    .vue({ version: 2 })
     .sass('resources/sass/app.scss', 'public/css');
+
+if (!mix.inProduction()) {
+    mix.sourceMaps()
+}
